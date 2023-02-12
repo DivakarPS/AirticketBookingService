21b2e0abb0a90608232949ceafc574f2cb8b6b32
Divakar <divaakaar.saakthi@gmail.com>
2023-02-12 19:30:55 +0530

    completed create booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -6,7 +6,8 @@ const {AppError , ValidationError} = require('../utils/errors/index');
 class BookingRepository {
     async create(data){
         try {
-            
+            const booking = await Booking.create(data);
+            return booking;
         } catch (error) {
             if(error.name == "SequelizeValidationError"){
                 throw new ValidationError(error);
@@ -15,7 +16,26 @@ class BookingRepository {
                 'Repository Error',
                 'Cannot create Booking',
                 'There was some issue creating the booking, please try again later',
-                StatusCodes.INTERNAL_SERVER_ERROR);
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    async update(bookingId,data){
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status){
+                booking.status = data.status;
+            }
+            booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'Repository Error',
+                'Cannot create Booking',
+                'There was some issue creating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
